38fc174b9e063eadadfcb0b3c0ce11c984ac7883
ganeshgowri-ASA <ganeshgowri.mitsui@gmail.com>
2026-03-09 06:37:27 +0530

    feat(pages): add /studio/animation page route

---
+++ src/app/studio/animation/page.tsx
@@ -0,0 +1,9 @@
+import AnimationStudio from "@/components/studio/AnimationStudio";
+
+export default function AnimationPage() {
+  return (
+    <div className="container mx-auto px-4 py-8 max-w-4xl">
+      <AnimationStudio />
+    </div>
+  );
+}
